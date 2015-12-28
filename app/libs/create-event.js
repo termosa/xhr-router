d2db7daa7ee245382115b69f50cf13009ec8d0f7
Stanislav Termosa <termosa.stanislav@gmail.com>
2015-12-28 09:39:37 +0200

    Remove  from loop

--- app/libs/create-event.js
+++ app/libs/create-event.js
@@ -1,16 +1,23 @@
 var window = require('device/window');
 var document = require('device/document');
 
+function applyCustomProperties (event, properties) {
+  var name, names = Object.keys(properties);
+
+  for (var i = props.length; i--;) {
+    if (name === 'bubbles' || name === 'cancelable') { continue; }
+    name = names[i];
+    event[name] = properties[name];
+  }
+
+  return event;
+}
+
 function createEventInOldWay (type, options) {
   options = options || {};
   var event = document.createEvent('Event');
   event.initEvent(type, options.bubbles, options.cancelable);
-  for (var name in options) {
-    if (name !== 'bubbles' && name !== 'cancelable') {
-      event[name] = options[name];
-    }
-  }
-  return event;
+  return applyCustomProperties(event);
 }
 
 function createEvent (type, options) {
